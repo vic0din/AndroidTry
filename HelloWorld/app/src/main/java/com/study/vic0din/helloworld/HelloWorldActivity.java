3f42c1999bc922f79ca8ee25cbe330e8fde7cbf2
vic0din <Ska-vlad007@yandex.ru>
2017-05-21 18:31:29 +0300

    added resources for vertical and horizontal states of screen

--- HelloWorld/app/src/main/java/com/study/vic0din/helloworld/HelloWorldActivity.java
+++ HelloWorld/app/src/main/java/com/study/vic0din/helloworld/HelloWorldActivity.java
@@ -8,6 +8,6 @@ public class HelloWorldActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hello_world);
+        setContentView(R.layout.activity_relative_layout);
     }
 }
