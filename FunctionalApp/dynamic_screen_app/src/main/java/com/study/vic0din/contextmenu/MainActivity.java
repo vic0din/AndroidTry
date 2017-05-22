17c949f19463bfac1b377e374e8248ae72d20f8c
vic0din <Ska-vlad007@yandex.ru>
2017-05-22 14:11:43 +0300

    using SeekBar

--- FunctionalApp/dynamic_screen_app/src/main/java/com/study/vic0din/contextmenu/MainActivity.java
+++ FunctionalApp/dynamic_screen_app/src/main/java/com/study/vic0din/contextmenu/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.*;
 /**
  * Created by BBPax on 22.05.17.
  */
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, SeekBar.OnSeekBarChangeListener {
     LinearLayout llMain;
     RadioGroup rgGravity;
     EditText etName;
@@ -17,6 +17,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button btnClear;
 
     int wrapContent = LinearLayout.LayoutParams.WRAP_CONTENT;
+
+    // TODO: 22.05.17   полоса прокрутки
+    SeekBar sbWeight;
+    Button btn1;
+    Button btn2;
+
+    LinearLayout.LayoutParams lParams1;
+    LinearLayout.LayoutParams lParams2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +40,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         btnClear = (Button) findViewById(R.id.btnClear);
         btnClear.setOnClickListener(this);
+
+        // TODO: 22.05.17   полоса прокрутки
+        sbWeight = (SeekBar) findViewById(R.id.sbWeight);
+        sbWeight.setOnSeekBarChangeListener(this);
+
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+
+        lParams1 = (LinearLayout.LayoutParams) btn1.getLayoutParams();
+        lParams2 = (LinearLayout.LayoutParams) btn2.getLayoutParams();
     }
 
     /**
@@ -75,5 +93,47 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Toast.makeText(this, "Удалено", Toast.LENGTH_SHORT).show();
                 break;
         }
+    }
+
+    /**
+     * Notification that the progress level has changed. Clients can use the fromUser parameter
+     * to distinguish user-initiated changes from those that occurred programmatically.
+     *
+     * @param seekBar  The SeekBar whose progress has changed
+     * @param progress The current progress level. This will be in the range 0..max where max
+     *                 was set by {@link ProgressBar#setMax(int)}. (The default value for max is 100.)
+     * @param fromUser True if the progress change was initiated by the user.
+     */
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        int leftValue = progress;
+        int rightValue = seekBar.getMax() - progress;
+        // настраиваем вес
+        lParams1.weight = leftValue;
+        lParams2.weight = rightValue;
+        // в текст кнопок пишем значения переменных
+        btn1.setText(String.valueOf(leftValue));
+        btn2.setText(String.valueOf(rightValue));
+    }
+
+    /**
+     * Notification that the user has started a touch gesture. Clients may want to use this
+     * to disable advancing the seekbar.
+     *
+     * @param seekBar The SeekBar in which the touch gesture began
+     */
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+    }
+
+    /**
+     * Notification that the user has finished a touch gesture. Clients may want to use this
+     * to re-enable advancing the seekbar.
+     *
+     * @param seekBar The SeekBar in which the touch gesture began
+     */
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
     }
 }
