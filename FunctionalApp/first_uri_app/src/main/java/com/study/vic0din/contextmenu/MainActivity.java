2c1097168bacc8c68e78b3c31a0af21481b49ad2
vic0din <Ska-vlad007@yandex.ru>
2017-05-23 13:43:53 +0300

    First URI in Android App

---
+++ FunctionalApp/first_uri_app/src/main/java/com/study/vic0din/contextmenu/MainActivity.java
@@ -0,0 +1,80 @@
+package com.study.vic0din.contextmenu;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.*;
+
+/**
+ * Created by BBPax on 22.05.17.
+ */
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    int wrapContent = LinearLayout.LayoutParams.WRAP_CONTENT;
+
+    Button btnWeb;
+    Button btnMap;
+    Button btnCall;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnWeb = (Button) findViewById(R.id.btnWeb);
+        btnMap = (Button) findViewById(R.id.btnMap);
+        btnCall = (Button) findViewById(R.id.btnCall);
+
+        btnWeb.setOnClickListener(this);
+        btnMap.setOnClickListener(this);
+        btnCall.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch (v.getId()) {
+            case R.id.btnWeb:
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://developer.android.com"));
+                startActivity(intent);
+                break;
+            case R.id.btnMap:
+                intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("geo:55.754283,37.62002"));
+                startActivity(intent);
+                break;
+            case R.id.btnCall:
+                intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:12345"));
+                startActivity(intent);
+                break;
+        }
+    }
+
+    // создание меню
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // обработка нажатий на пункты меню
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_exit:
+                // выход из приложения
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
