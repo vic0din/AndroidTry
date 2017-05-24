c36d36a07119974a1476c7993d2ebd58905cbb7d
vic0din <Ska-vlad007@yandex.ru>
2017-05-24 15:38:38 +0300

    new project study and empty_app

---
+++ Study/empty_app/src/main/java/com/study/vic0din/study/MainActivity.java
@@ -0,0 +1,48 @@
+package com.study.vic0din.study;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.*;
+
+/**
+ * Created by BBPax on 22.05.17.
+ */
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    int wrapContent = LinearLayout.LayoutParams.WRAP_CONTENT;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    // создание меню
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // обработка нажатий на пункты меню
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_exit:
+                // выход из приложения
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
